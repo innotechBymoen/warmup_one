4508be242a88001f0d845ccfb4ef0bd04c2aaec8
Katelyn Lehner <katelynlehner@Katelyns-MacBook-Air.local>
2021-12-19 22:41:08 -0600

    Ready for students! Have fun :)

---
+++ src/components/BookOption.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <!-- TODO-3 I want to display the book information up here in a nice card -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BookOption',
+  // TODO-3 I should probably expect some data from the parent here so define the book
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Books!</h1>
+    <!-- TODO-2 I want to loop through all my books and create a BookOption for each one -->
+    <book-option></book-option>
+  </div>
+</template>
+
+<script>
+import BookOption from '@/components/BookOption.vue';
+
+export default {
+  name: 'HomePage',
+  components: {
+    BookOption
+  },
+  // TODO-1 I want to make an axios request when this page loads to the API: https://fakerapi.it/api/v1/books
+  // I know that I can give the parameter _quantity to select how many bookes I want, and I want 5
+  // When I get the 5 books, I need to but them in a local variable in this component
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
